37f2e9150aedc1ed092ca6485f33971efb40957b
Alejandro Ramos <alejandro.ramos.work@gmail.com>
2018-03-17 17:08:55 -0400

    Clean up the code a bit (a lot)
    It should be a lot easier to read since it cuts back on global variables
    a lot. It could probably still use some more work, but this should
    suffice for now.

--- rsdcs.js
+++ rsdcs.js
@@ -1,4 +1,4 @@
-/*
+/**
     This file is part of RuneScape-Discord Chat Sync
     Copyright (C) 2018 Alejandro Ramos
 
@@ -16,97 +16,104 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-const puppeteer = require('puppeteer');
-const Discord = require('discord.js');
+const puppeteer = require("puppeteer");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 const config = require("./config.json");
 
 var readyToSend = false;
-var lastIndex = {number: -1};
-var intervalID;
+const lastIndex = {number: -1};
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+(async() => {
+    await console.log(getDateTime() + ": Started program");
+    await client.login(config.login.discord);
+    const browser = await puppeteer.launch({args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-web-security", "--user-data-dir"]});
+    let page = await browser.newPage();
+    await startup(page);
+})();
 
-async function click(frame, selector) {
-    let x = await frame.$(selector); // unfortunately i have to make a new variable for this (for some reason)
-    await x.click();
-}
-
-async function startup(browser, page) {
-    if (intervalID !== undefined) {
-        clearInterval(intervalID);
-    }
-
+async function startup(page) {
     lastIndex.number = -1;
 
-    await page.goto('http://www.runescape.com/companion/comapp.ws');
-    await console.log(getDateTime() + ": " + "Loaded page");
+    await page.goto("http://www.runescape.com/companion/comapp.ws");
+    await console.log(getDateTime() + ": Loaded page");
     let frame = await page.frames()[1];
 
     try {
         await frame.waitForSelector("body:not(.initial-load)", {timeout: 10000});
     } catch (e) {
         await console.log(getDateTime() + ": Took too long to load");
-        await restart(browser, page);
+        await restart(page);
         return;
     }
-    await console.log(getDateTime() + ": " + "Fully loaded page");
+    await console.log(getDateTime() + ": Fully loaded page");
 
     await frame.type("input#username", config.login.username); // type the username
     await frame.type("input#password", config.login.password); // type the password
-    await click(frame, "button.icon-login"); // click on the submit button
+    await frame.click("button.icon-login"); // click on the submit button
     await frame.waitForSelector("div.modal-body.ng-scope");
-    await console.log(getDateTime() + ": " + "Logged in");
+    await console.log(getDateTime() + ": Logged in");
 
-    await click(frame, "a[ng-click='modalCancel()']"); // click on the 'no' button on the save password dialog
-    await frame.waitForSelector("div.modal-body.ng-scope", {"hidden":true});
-    await console.log(getDateTime() + ": " + "In app");
+    await frame.click("a[ng-click='modalCancel()']"); // click on the "no" button on the save password dialog
+    await frame.waitForSelector("div.modal-body.ng-scope", {"hidden": true});
+    await console.log(getDateTime() + ": In app");
 
-    await click(frame, "li.all-chat"); // click on the chat tab
+    await frame.click("li.all-chat"); // click on the chat tab
     await frame.waitForSelector("section.chat.all-chat.ng-scope");
     await sleep(250); // wait for the slider to show it
-    await console.log(getDateTime() + ": " + "In chat tab");
+    await console.log(getDateTime() + ": In chat tab");
 
     if (config.configs.chatType === "clan") {
-        await click(frame, "i.icon-clanchat:not(.icon)"); // click on the clan chat tab
+        await frame.click("i.icon-clanchat:not(.icon)"); // click on the clan chat tab
     } else if (config.configs.chatType === "friends") {
-        await click(frame, "i.icon-friendschat:not(.icon)"); // click on the friends chat tab
+        await frame.click("i.icon-friendschat:not(.icon)"); // click on the friends chat tab
     } else {
-        await console.log(getDateTime() + ": " + "Not a valid chatType. must be 'clan' or 'friends'");
+        await console.log(getDateTime() + ": Not a valid chat type. must be \"clan\" or \"friends\"");
         await shutdown();
     }
     await frame.waitForSelector("input#message");
-    await console.log(getDateTime() + ": " + "In " + config.configs.chatType + " chat tab");
+    await console.log(getDateTime() + ": In " + config.configs.chatType + " chat tab");
     readyToSend = true;
-    await console.log(getDateTime() + ": " + "Ready to chat!");
+    await console.log(getDateTime() + ": Ready to chat!");
 
     // Everything is ready, so enable the chat features
-    await read(browser, page, client, frame, lastIndex);
-    intervalID = await setInterval(read, 500, browser, page, client, frame, lastIndex);
+    async function handleRead(){
+        const output = await read(page, lastIndex);
+        lastIndex.number = output[1];
+        if (output[0] === "disconnected") {
+            readyToSend = false;
 
-    client.on("message", msg => {
+            await console.log(getDateTime() + ": Lost connection");
+            try {
+                await frame.waitForSelector("div.modal-body.ng-scope", {timeout: 5000});
+            } catch (e) {
+                await console.log(getDateTime() + ": Unexpected error, screenshotting");
+                await page.screenshot({path: "./" + getDateTime() + ": error1" + ".png"});
+            }
+            await restart(page);
+        } else if (output[0] !== "clear") {
+            await client.channels.get(config.configs.channelID).send(output[0]); // send the message in the discord
+            setTimeout(handleRead, 0);
+        } else {
+            setTimeout(handleRead, 600);
+        }
+    }
+    setTimeout(handleRead, 0);
+
+    client.on("message", message => {
         if (readyToSend) {
-            if (msg.channel.id == config.configs.channelID && msg.author.id !== config.configs.botID) {
-                let author = msg.member.nickname;
+            if (message.channel.id == config.configs.channelID && message.author.id !== config.configs.botID) {
+                let author = message.member.nickname;
                 if (author == null) {
-                    author = msg.author.username;
+                    author = message.author.username;
                 }
-                send(page, msg.content, author, frame);
+                send(page, message.content, author, frame);
             }
         }
     });
 }
 
-(async() => {
-    await console.log(getDateTime() + ": " + "Started program");
-    await client.login(config.login.discord);
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-web-security', '--user-data-dir']});
-    let page = await browser.newPage();
-    await startup(browser, page);
-})();
 
 async function send(page, message, author, frame) {
     // if the message is too long to send in runescape
@@ -116,13 +123,13 @@ async function send(page, message, author, frame) {
         await send(page, message.substring((80 - author.length - 2), message.length), author, frame); // send the rest of the message
     } else {
         await frame.type("input#message", author + ": " + message);
-        await click(frame, "input[type='submit']"); // click on the send button
+        await frame.click("input[type='submit']"); // click on the send button
     }
 }
 
-async function read(browserr, pagee, clientt, frame, lastIndex) {
+async function read(page, lastIndex) {
     if (readyToSend) {
-        let output = await pagee.evaluate(function (lastIndex) {
+        return await page.evaluate((lastIndex) => {
             function getNextMessage(ul, lastIndex) {
                 let list = ul.querySelectorAll("li.message.clearfix.ng-scope:not(.my-message):not(.historical)");
                 if (lastIndex.number < list.length - 1) {
@@ -134,7 +141,7 @@ async function read(browserr, pagee, clientt, frame, lastIndex) {
 
             let div = window.frames[0].document.getElementsByClassName("content push-top-double push-bottom-double").item(0); // the div that holds the list
             if (div != null) {
-                let ul = div.getElementsByTagName('ul').item(0); // the list
+                let ul = div.getElementsByTagName("ul").item(0); // the list
                 if (ul != null) { // if there are messages
                     let lastMessage = getNextMessage(ul, lastIndex);
 
@@ -147,54 +154,41 @@ async function read(browserr, pagee, clientt, frame, lastIndex) {
                             let message = messageElement.innerHTML; // the actual content of the message
 
                             let time = new Date();
-                            time = ('0' + time.getUTCHours()).slice(-2) + ":" + ('0' + time.getUTCMinutes()).slice(-2) + ":" + ('0' + time.getUTCSeconds()).slice(-2);
+                            time = ("0" + time.getUTCHours()).slice(-2) + ":" + ("0" + time.getUTCMinutes()).slice(-2) + ":" + ("0" + time.getUTCSeconds()).slice(-2);
                             return [(time + ": " + author + ":\n```" + message + "```"), lastIndex.number];
                         }
                     }
                 }
             } else {
-                return ["null", lastIndex.number]; // there was an error and the bot is no longer in the chat screen
+                return ["disconnected", lastIndex.number]; // there was an error and the bot is no longer in the chat screen
             }
-            return ["undefined", lastIndex.number]; // there are no messages to read, so just send nothing
+            return ["clear", lastIndex.number]; // there are no messages to read, so just send nothing
         }, lastIndex);
-        lastIndex.number = output[1];
-        if (output[0] !== "null" && output[0] !== "undefined") {
-            readyToSend = true;
-            await clientt.channels.get(config.configs.channelID).send(output[0]); // send the message in the discord
-        } else if (output[0] === "null") {
-            readyToSend = false;
-            await error1(browserr, pagee, clientt, frame);
-        }
+    } else {
+        return ["clear", lastIndex.number];
     }
 }
 
-async function error1(browserr, page, clientt, frame) {
-    await console.log(getDateTime() + ": Lost connection");
-    try {
-        await frame.waitForSelector("div.modal-body.ng-scope", {timeout: 5000});
-    } catch(e) {
-        await page.screenshot({path: "./" + getDateTime() + ": " + "error1" + ".png"});
-        await console.log(getDateTime() + ": Unexpected error (error1)");
-    }
-    await restart(browserr, page);
+function restart(page) {
+    console.log(getDateTime() + ": Restarting...\n");
+    startup(page);
 }
-
-function restart(browserr, page) {
-    console.log(getDateTime() + ": " + "Restarting...\n");
-    startup(browserr, page);
-}
-
-process.on('SIGINT', () => {
-    shutdown()
-});
 
 function shutdown() {
-    console.log(getDateTime() + ": Shutting down!");
+    console.log("\n" + getDateTime() + ": Shutting down!");
     client.destroy();
     process.exit();
 }
 
+process.on("SIGINT", () => {
+    shutdown();
+});
+
 function getDateTime() {
     let date = new Date();
-    return date.getUTCFullYear() + ":" + ('0' + (date.getUTCMonth() + 1)).slice(-2) + ":" + ('0' + date.getUTCDate()).slice(-2) + ":" + ('0' + date.getUTCHours()).slice(-2) + ":" + ('0' + date.getUTCMinutes()).slice(-2) + ":" + ('0' + date.getUTCSeconds()).slice(-2);
+    return date.getUTCFullYear() + ":" + ("0" + (date.getUTCMonth() + 1)).slice(-2) + ":" + ("0" + date.getUTCDate()).slice(-2) + ":" + ("0" + date.getUTCHours()).slice(-2) + ":" + ("0" + date.getUTCMinutes()).slice(-2) + ":" + ("0" + date.getUTCSeconds()).slice(-2);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
